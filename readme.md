f1377d0b9b92e091b0d8d9842c9026bc55ee0583
Wireless4024 <wireless4024@proton.me>
2022-10-22 04:28:53 +0700

    add file via upload

---
+++ readme.md
@@ -0,0 +1,54 @@
+# Hello
+Hello.
+
+Today hacker just want my steam credential so I send a lot of account to hacker. I hope hacker is very happy about this.  
+
+
+---
+
+# About this project
+
+This project designed to send huge amount of dynamic payload to http endpoint.
+
+## Features
+
++ Change body on each request
++ Customizable site parser (if they change backend url frequently)
++ Rate limit per sec
++ Parallel request support (by default it only spawn concurrent tasks)
++ Exit after server fail
+
+## Run this project
+```shell
+git clone https://github.com/Wireless4024/hello
+```
+
+# TLDR;
+
++ I didn't lose anything just sanity.
++ Don't log in to untrusted website.
+  > If the website ask for login please open another tab and search for official website in google then refresh the page
+  to login (If it still has login ui mean its FAKE!).
++ Fake website that I found is already shutdown.
+
+---
+
+# Story
+
+Hello today I got message from someone from steam.
+
+At the beginning he said he want someone to join dota2 tournament team,  
+surely I said ok, and he sends a link from challenger-mode to his team and he said did you see join button?  
+But I didn't see anything so I ask him again and then he sends (currently 404) to me (First time I guess maybe
+challenger-mode just generate that link for him)
+
+UI part I think he just buy this website template from someone or maybe free from github.
+
+Bad news is I put my credential into that website :( with steam guard and recovery code.
+After I have known I got hacked because I have family mode turned on and they bruteforce and remove it (its gone from
+steam ui), my steam session is logged out and I got chat message from fake steam user (their script add this user after
+bruteforce family mode),  
+Good news is they can't change password because it needs to be confirmed by mobile app  
+BUT they have my steam guard recovery code!  so I quickly grab my phone and take my steam guard back to my phone, and
+they just spam password reset confirmation to my steam mobile app, and I can't even change my password :(  
+about 3 minutes later I'm able to change my password and I guess I'm safe now.
---
+++ src/main.rs
@@ -0,0 +1,199 @@
+
+use std::collections::HashMap;
+use std::error::Error;
+use std::time::{Duration, Instant};
+
+use bstr::ByteSlice;
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
+use rand::Rng;
+use reqwest::Client;
+use reqwest::redirect::Policy;
+use tokio::time::sleep;
+
+/// Number of parallel runtime(s)
+static mut PARALLEL: usize = 1;
+/// Limit number of request per sec per PARALLEL
+static mut REQ_PER_SEC: usize = 128;
+/// request count per PARALLEL (exit after reached 0 for unlimited)
+static mut COUNTS: usize = 1_000_000;
+/// use http2 only
+static mut USE_H2: bool = false;
+/// url to fake page
+static mut HOST: &str = "";
+
+/// Change this function to edit body on each request
+fn transform_body(mut h: HashMap<&'static str, String>) -> HashMap<&'static str, String> {
+	h.insert("accountName", rand_text(128));
+	h.insert("password", rand_text(64));
+	if !h.contains_key("domain") {
+		h.insert("domain", unsafe { HOST }.to_string());
+	}
+	h
+}
+
+/// Change this function to edit how to parse the site
+async fn parse_site(url: &'static str) -> Result<Option<Site>, Box<dyn Error>> {
+	let resp = reqwest::get(url).await?;
+	if !resp.status().is_success() {
+		return Ok(None);
+	}
+	let bytes = resp.bytes().await?;
+	let mut site: &[u8] = &[];
+	for x in bytes.as_bstr().lines() {
+		if x.trim().starts_with(b"window.$loginLink") {
+			let left = x.find(b"\"");
+			let right = x.rfind(b"\"");
+			if let (Some(left), Some(right)) = (left, right) {
+				if left == right {
+					return Ok(None);
+				}
+				site = &x[left + 1..right];
+			}
+		}
+	}
+	Ok(Some(Site { url: site.as_bstr().to_string(), referer: url }))
+}
+
+fn main() {
+	init();
+}
+
+const CHARS: [char; 36] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+fn rand_text(len: usize) -> String {
+	let mut str = String::with_capacity(len);
+	let mut rng = rand::thread_rng();
+	for _ in 0..len {
+		str.push(CHARS[rng.gen_range(0..CHARS.len())]);
+	}
+	str
+}
+
+fn new_client() -> Client {
+	let mut builder = Client::builder()
+		.redirect(Policy::none());
+	if unsafe { USE_H2 } {
+		builder = builder.http2_prior_knowledge().use_rustls_tls()
+	}
+	return builder.build().unwrap();
+}
+
+fn init() {
+	let parallel = unsafe { PARALLEL };
+	let runtime = tokio::runtime::Builder::new_current_thread()
+		.enable_all()
+		.global_queue_interval(31)
+		.build()
+		.unwrap();
+
+	let runtimes = runtime.block_on(async move {
+		let site = parse_site(unsafe { HOST }).await.unwrap();
+		if site.is_none() {
+			eprintln!("Can't parse from this site!");
+			return Vec::new();
+		}
+		let site = site.unwrap();
+		println!("Using config: {:#?}", site);
+		let url: &'static mut str = Box::leak(site.url.into_boxed_str());
+		let referer = site.referer;
+		let fut = FuturesUnordered::new();
+		let mut runtimes = Vec::with_capacity(parallel);
+		for _ in 0..parallel {
+			let rt = tokio::runtime::Builder::new_multi_thread()
+				.enable_all()
+				.global_queue_interval(31)
+				.worker_threads(2)
+				.build()
+				.unwrap();
+			let f = say_hello(new_client(), url, referer, unsafe { COUNTS }, transform_body);
+			let f = rt.spawn(f);
+			fut.push(f);
+			runtimes.push(rt);
+		}
+		let _: Vec<_> = fut.collect().await;
+		runtimes
+	});
+
+	for r in runtimes {
+		r.shutdown_background();
+	}
+	runtime.shutdown_background();
+}
+
+async fn say_hello<F>(client: Client, url: &'static str, referer: &'static str, limit: usize, transform_body: F)
+	where F: Fn(HashMap<&'static str, String>) -> HashMap<&'static str, String> {
+	let mut i: usize = 0;
+	let chunk_size = (1 << unsafe { REQ_PER_SEC as f32 }.log2().ceil() as usize) - 1;
+	println!("using chunk size = {:?}", chunk_size);
+
+	let mut futures = FuturesUnordered::new();
+	let rate = unsafe { REQ_PER_SEC };
+	let mut last = Instant::now();
+	let mut body = transform_body(HashMap::new());
+	let prep = client.post(url)
+		.header("Sec-Ch-Ua", r#""Chromium";v="105", "Not)A;Brand";v="8""#)
+		.header("User-Agent", r#"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.127 Safari/537.36"#)
+		.header("Origin", referer)
+		.header("Referer", referer);
+	loop {
+		let f = tokio::spawn(prep.try_clone()
+			.unwrap()
+			.form(&body)
+			.send());
+		body = transform_body(body);
+		futures.push(f);
+		if i & chunk_size == 0 {
+			if i != 0 {
+				println!("waiting {} task to fulfill.", i);
+				let result: Vec<_> = futures.collect().await;
+
+				let err = result.into_iter().find(|it| {
+					return if it.is_err() { true } else {
+						let it = it.as_ref().unwrap();
+						match it {
+							Ok(resp) => {
+								!resp.status().is_success()
+							}
+							Err(_) => {
+								true
+							}
+						}
+					};
+				});
+				if let Some(err) = err {
+					println!("Found error stopping");
+					println!("{:#?}", err);
+					futures = FuturesUnordered::new();
+					break;
+				}
+				futures = FuturesUnordered::new();
+			}
+			println!("spawning task {}", i + 1);
+		}
+		i += 1;
+		if i == limit {
+			break;
+		}
+		if rate != 0 {
+			let diff = Instant::now().duration_since(last);
+			if diff.as_secs() < 1 && (i % rate) == 0 && i != 0 {
+				println!("Limiting {}req/s ({}ms)", rate, diff.as_millis());
+				sleep(Duration::from_millis((1000 - diff.as_millis()) as u64)).await;
+				last = Instant::now();
+			}
+		}
+	};
+	let result: Vec<_> = futures.collect().await;
+	let err = result.into_iter().find(|it| it.is_err() || it.as_ref().unwrap().is_err());
+	if let Some(err) = err {
+		println!("Found error stopping");
+		println!("{:#?}", err);
+	}
+}
+
+#[derive(Debug)]
+struct Site {
+	url: String,
+	referer: &'static str,
+}
